a748a502e25b539402354d677c79e3eea48cc320
Drorka~ <drorpa4@gmail.com>
2023-01-19 17:26:22 +0200

    support edit group title

--- Frontend/src/cmps/group-list.jsx
+++ Frontend/src/cmps/group-list.jsx
@@ -7,46 +7,61 @@ import { TaskList } from './task-list'
 
 export function GroupList({ onRemoveGroup }) {
 	const board = useSelector((storeState) => storeState.boardModule.board)
-	const [isAddNewListOpen, setIsAddNewListOpen] = useState(false)
+	const [isAddNewGroupOpen, setIsAddNewGroupOpen] = useState(false)
 	const [groupToEdit, setGroupToEdit] = useState(boardService.getEmptyGroup())
 	const groups = board.groups
 
-	let addNewListOpenClosed = isAddNewListOpen ? 'open' : 'closed'
-	function onToggleAddCancelList() {
-		setIsAddNewListOpen(!isAddNewListOpen)
+	let addNewGroupOpenClosed = isAddNewGroupOpen ? 'open' : 'closed'
+	function onToggleAddCancelGroup() {
+		setIsAddNewGroupOpen(!isAddNewGroupOpen)
+	}
+	function closeAddNewGroup() {
+		setIsAddNewGroupOpen(false)
 	}
 
-	function handleListChange({ target }) {
-		let { value, name: field } = target
-		setGroupToEdit((prevGroup) => ({ ...prevGroup, [field]: value }))
+	function handleGroupChange({ target }) {
+		// check if it's an existing group or a new one
+		if (target.id) {
+			setGroupToEdit(getGroupToEdit(target.id))
+		} else {
+			let { value, name: field } = target
+			setGroupToEdit((prevGroup) => ({ ...prevGroup, [field]: value }))
+		}
 	}
 
-	async function onSubmit(ev) {
+	function getGroupToEdit(groupId) {
+		const group = groups.find((grp) => grp.id === groupId)
+		return group
+	}
+
+	async function onSaveGroup(ev) {
 		ev.preventDefault()
 		try {
 			await saveGroup(groupToEdit, board._id)
-			onToggleAddCancelList()
+			closeAddNewGroup()
 			setGroupToEdit(boardService.getEmptyGroup())
 		} catch (err) {
 			console.log('Failed to save group', err)
 		}
 	}
 
-	function onAddTask(ev) {
-		ev.preventDefault()
-		console.log('ev', ev)
-		console.log('add new task to group')
-	}
-
 	if (!groups) return <h1>Loading....</h1>
-
 	return (
 		<section className="group-list-container">
 			<ul className="group-list clean-list">
 				{groups.map((group) => (
 					<li className="group-wrapper" key={group.id}>
-						<div className="group-top">
-							<h2 className="group-title">{group.title}</h2>
+						<div className="group-top open">
+							<form onBlurCapture={onSaveGroup}>
+								<textarea
+									name="title"
+									className="edit-group-title"
+									id={group.id}
+									maxLength="512"
+									defaultValue={group.title}
+									onChange={handleGroupChange}
+								></textarea>
+							</form>
 							<button
 								className="btn-group"
 								onClick={() => {
@@ -64,21 +79,20 @@ export function GroupList({ onRemoveGroup }) {
 					</li>
 				))}
 
-				<div className={`add-new-group ${addNewListOpenClosed}`}>
-					<form onSubmit={onSubmit}>
+				<div className={`add-new-group ${addNewGroupOpenClosed}`}>
+					<form onSubmit={onSaveGroup} onBlurCapture={closeAddNewGroup}>
 						<input
 							type="text"
 							name="title"
-							id="title"
 							placeholder="Enter list title..."
 							maxLength="512"
 							value={groupToEdit.title}
-							onChange={handleListChange}
+							onChange={handleGroupChange}
 						/>
 						<div
 							className="placeholder"
 							onClick={() => {
-								onToggleAddCancelList()
+								onToggleAddCancelGroup()
 							}}
 						>
 							+ Add another list
@@ -91,7 +105,7 @@ export function GroupList({ onRemoveGroup }) {
 								href="#"
 								className="btn-cancel-list"
 								onClick={() => {
-									onToggleAddCancelList()
+									onToggleAddCancelGroup()
 								}}
 							>
 								X
