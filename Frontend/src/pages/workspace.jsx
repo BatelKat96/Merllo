5e289071ac3f54657aa33baf2fdc2469a0777028
Drorka~ <drorpa4@gmail.com>
2023-01-20 19:39:21 +0200

    design workspace page

--- Frontend/src/pages/workspace.jsx
+++ Frontend/src/pages/workspace.jsx
@@ -1,27 +1,67 @@
 import { Link } from 'react-router-dom'
+import { AiOutlineStar, AiOutlineClockCircle } from 'react-icons/ai'
+import { HiOutlineStar } from 'react-icons/hi2'
 
 export function Workspace() {
-    console.log('wow:')
+	console.log('wow:')
 
-    return <section className='workspace-section'>
-        <div className='starred-boards'>
-            <h1>Starred boards</h1>
-            <ul className='board-list clean-list'>
-                <Link to="/board/b101"><li className='board-preview board-b101'>Demo</li></Link>
-                <Link to="/board/b102"><li className='board-preview board-b102'></li></Link>
-                <Link to="/board/b103"><li className='board-preview board-b102'></li></Link>
+	function createBoard() {
+		console.log('create new board')
+	}
 
-            </ul>
-        </div>
-        <div className='recently-viewed-boards'>
-            <h1>Recently viewed</h1>
-            <ul className='board-list clean-list'>
-                <Link to="/board/b101"><li className='board-preview board-b101'>Demo</li></Link>
-                <Link to="/board/b102"><li className='board-preview board-b102'></li></Link>
-                <Link to="/board/b103"><li className='board-preview board-b103'></li></Link>
+	return (
+		<section className="workspace-section">
+			<div className="starred-boards">
+				<div className="boards-section-header">
+					<div className="boards-section-header-icon">
+						<HiOutlineStar />
+					</div>
+					<div className="boards-section-header-name">
+						<h3>Starred boards</h3>
+					</div>
+				</div>
 
+				<ul className="board-list clean-list">
+					<Link to="/board/b101">
+						<li className="board-preview board-b101">Demo</li>
+					</Link>
+					<Link to="/board/b102">
+						<li className="board-preview board-b102"></li>
+					</Link>
+					<Link to="/board/b103">
+						<li className="board-preview board-b102"></li>
+					</Link>
+				</ul>
+			</div>
 
-            </ul>
-        </div>
-    </section>
+			<div className="recently-viewed-boards">
+				<div className="boards-section-header">
+					<div className="boards-section-header-icon">
+						<AiOutlineClockCircle />
+					</div>
+					<div className="boards-section-header-name">
+						<h3>Recently viewed</h3>
+					</div>
+				</div>
+
+				<ul className="board-list clean-list">
+					<li
+						className="board-preview create-new-board"
+						onClick={() => createBoard()}
+					>
+						<span>Create new board</span>
+					</li>
+					<Link to="/board/b101">
+						<li className="board-preview board-b101">Demo</li>
+					</Link>
+					<Link to="/board/b102">
+						<li className="board-preview board-b102"></li>
+					</Link>
+					<Link to="/board/b103">
+						<li className="board-preview board-b103"></li>
+					</Link>
+				</ul>
+			</div>
+		</section>
+	)
 }
