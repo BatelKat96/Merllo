f49e65d10049c6726e12c4aee4c66868acce4198
Drorka~ <drorpa4@gmail.com>
2023-01-22 17:21:45 +0200

    workspace design fixes

--- Frontend/src/pages/workspace.jsx
+++ Frontend/src/pages/workspace.jsx
@@ -2,11 +2,12 @@ import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { loadBoards, updateBoard } from '../store/board.actions'
 import { boardService } from '../services/board.service'
+import { BoardCreate } from '../cmps/board-create'
+import { BoardPreview } from '../cmps/board-preview'
 
 import { AiOutlineClockCircle } from 'react-icons/ai'
 import { HiOutlineStar } from 'react-icons/hi2'
-import { BoardCreate } from '../cmps/board-create'
-import { BoardPreview } from '../cmps/board-preview'
+import { ReactComponent as UserSvg } from '../assets/img/icons-header/user.svg'
 
 export function Workspace() {
 	const boards = useSelector((storeState) => storeState.boardModule.boards)
@@ -59,64 +60,83 @@ export function Workspace() {
 
 	return (
 		<section className="workspace-section">
-			<div className="starred-boards">
+			{starredBoards.length !== 0 && (
+				<div className="starred-boards">
+					<div className="boards-section-header">
+						<div className="boards-section-header-icon">
+							<HiOutlineStar />
+						</div>
+						<div className="boards-section-header-name">
+							<h3>Starred boards</h3>
+						</div>
+					</div>
+
+					<ul className="board-list clean-list">
+						{starredBoards.map((strdBoard) => {
+							return (
+								<li key={`starred-${strdBoard._id}`}>
+									<a href={`/board/${strdBoard._id}`}>
+										<BoardPreview
+											board={strdBoard}
+											onToggleStar={onToggleStar}
+										/>
+									</a>
+								</li>
+								// <a href={`/board/${strdBoard._id}`}>
+								// 	<li
+								// 		className={`board-preview`}
+								// 		key={`starred-${strdBoard._id}`}
+								// 		style={boardStyle}
+								// 	>
+								// 		{strdBoard.title}
+								// 	</li>
+								// </a>
+
+								// <Link to={`/board/${strdBoard._id}`}>
+								// 	<li
+								// 		className={`board-preview`}
+								// 		key={`starred-${strdBoard._id}`}
+								// 		style={boardStyle}
+								// 	>
+								// 		{strdBoard.title}
+								// 	</li>
+								// </Link>
+							)
+						})}
+					</ul>
+				</div>
+			)}
+
+			<div className="my-boards">
 				<div className="boards-section-header">
 					<div className="boards-section-header-icon">
-						<HiOutlineStar />
+						<UserSvg />
 					</div>
 					<div className="boards-section-header-name">
-						<h3>Starred boards</h3>
+						<h3>Your boards</h3>
 					</div>
 				</div>
 
 				<ul className="board-list clean-list">
-					{starredBoards.map((strdBoard) => {
-						return (
-							<li key={`starred-${strdBoard._id}`}>
-								<a href={`/board/${strdBoard._id}`}>
-									<BoardPreview board={strdBoard} onToggleStar={onToggleStar} />
-								</a>
-							</li>
-							// <a href={`/board/${strdBoard._id}`}>
-							// 	<li
-							// 		className={`board-preview`}
-							// 		key={`starred-${strdBoard._id}`}
-							// 		style={boardStyle}
-							// 	>
-							// 		{strdBoard.title}
-							// 	</li>
-							// </a>
-
-							// <Link to={`/board/${strdBoard._id}`}>
-							// 	<li
-							// 		className={`board-preview`}
-							// 		key={`starred-${strdBoard._id}`}
-							// 		style={boardStyle}
-							// 	>
-							// 		{strdBoard.title}
-							// 	</li>
-							// </Link>
-						)
-					})}
+					{boards.map((board) => (
+						<li key={`your-${board._id}`}>
+							<a href={`/board/${board._id}`}>
+								<BoardPreview board={board} onToggleStar={onToggleStar} />
+							</a>
+						</li>
+					))}
+					<li
+						className="board-preview create-new-board"
+						onClick={openBoardComposer}
+						key="001"
+					>
+						<span>Create new board</span>
+					</li>
 				</ul>
 
-				{/* <ul className="board-list clean-list">
-					{starredBoards.map((strdBoard) => {
-						let boardStyle = getBoardStyle(strdBoard)
-						console.log(boardStyle)
-						return (
-							<Link to={`/board/${strdBoard._id}`}>
-								<li
-									className={`board-preview`}
-									key={`starred-${strdBoard._id}`}
-									style={boardStyle}
-								>
-									{strdBoard.title}
-								</li>
-							</Link>
-						)
-					})}
-				</ul> */}
+				{isBoardComposerOpen && (
+					<BoardCreate closeBoardComposer={closeBoardComposer} />
+				)}
 			</div>
 
 			<div className="recently-viewed-boards">
@@ -130,33 +150,14 @@ export function Workspace() {
 				</div>
 
 				<ul className="board-list clean-list">
-					<li
-						className="board-preview create-new-board"
-						onClick={openBoardComposer}
-						key="001"
-					>
-						<span>Create new board</span>
-					</li>
 					{boards.map((board) => (
-						<li key={`starred-${board._id}`}>
+						<li key={`recent-${board._id}`}>
 							<a href={`/board/${board._id}`}>
 								<BoardPreview board={board} onToggleStar={onToggleStar} />
 							</a>
 						</li>
-						// <Link to={`/board/${board._id}`}>
-						// 	<li
-						// 		className={`board-preview board-b101`}
-						// 		key={`viewed-${board._id}`}
-						// 	>
-						// 		{board.title}
-						// 	</li>
-						// </Link>
 					))}
 				</ul>
-
-				{isBoardComposerOpen && (
-					<BoardCreate closeBoardComposer={closeBoardComposer} />
-				)}
 			</div>
 		</section>
 	)
