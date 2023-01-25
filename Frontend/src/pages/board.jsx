6e2bb22e3d7ceac20544179cc3e83e1e0edc2864
Drorka~ <drorpa4@gmail.com>
2023-01-25 15:55:59 +0200

    support dark & light mode in board top menu

--- Frontend/src/pages/board.jsx
+++ Frontend/src/pages/board.jsx
@@ -101,6 +101,17 @@ export function Board() {
 		return { backgroundColor: `#0067a3` }
 	}
 
+	function getBoardTxtStyle(backgroundColor) {
+		console.log(backgroundColor)
+		var r = parseInt(backgroundColor.substring(1, 3), 16)
+		var g = parseInt(backgroundColor.substring(3, 5), 16)
+		var b = parseInt(backgroundColor.substring(5, 7), 16)
+		var yiq = (r * 299 + g * 587 + b * 114) / 1000
+		console.log('yiq', yiq)
+		console.log('yiq >= 128', yiq >= 128)
+		return yiq >= 128 ? 'light-bg' : 'dark-bg'
+	}
+
 	if (!board)
 		return (
 			<div className="loader-wrapper">
@@ -108,10 +119,11 @@ export function Board() {
 			</div>
 		)
 	const boardStyle = getBoardStyle()
+	const txtStyle = getBoardTxtStyle(board.style.backgroundColor)
 
 	return (
 		<section className="board" style={boardStyle}>
-			<div className="board-top-menu">
+			<div className={`board-top-menu-${txtStyle}`}>
 				<div className="board-top-menu-left">
 					<div className="board-title">
 						<input
