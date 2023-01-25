ab48b38fe23a948f626010196ff7188fe23bfec9
Drorka~ <drorpa4@gmail.com>
2023-01-26 00:15:45 +0200

    support task due date - set and remove

---
+++ Frontend/src/cmps/task-details-cmp/task-cmp-dynamic-modals/task-dates-modal.jsx
@@ -0,0 +1,77 @@
+import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { BiCheck } from 'react-icons/bi'
+
+import TextField from '@mui/material/TextField'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
+
+export function TaskDatesModal({ task, data, onSaveTask, onClose }) {
+	const board = useSelector((storeState) => storeState.boardModule.board)
+	const [selectedDate, setSelectedDate] = useState(new Date())
+
+	const onAddDueDate = (ev) => {
+		const dueDate = new Date(selectedDate).getTime()
+		console.log(dueDate)
+		task.dueDate = dueDate
+		onSaveTask(ev, task)
+		onClose()
+	}
+
+	const onRemoveDueDate = (ev) => {
+		console.log(task.dueDate)
+		task.dueDate = null
+		console.log(task.dueDate)
+		onSaveTask(ev, task)
+		onClose()
+	}
+
+	return (
+		<>
+			<section className="dates">
+				<LocalizationProvider
+					className="calendar"
+					style={{ margin: '-8px 0 0 0' }}
+					dateAdapter={AdapterDayjs}
+				>
+					<StaticDatePicker
+						disableToolbar
+						style={{ margin: '-8px 0 0 0' }}
+						width="300px"
+						height="300px"
+						margin="-8px 0 0 0"
+						displayStaticWrapperAs="desktop"
+						outsideCurrentMonth={true}
+						closeOnSelect={false}
+						openTo="day"
+						views={['month', 'day']}
+						value={selectedDate}
+						onChange={setSelectedDate}
+						renderInput={(params) => <TextField {...params} />}
+					/>
+				</LocalizationProvider>
+
+				<div className="dates-actions">
+					<button
+						onClick={(event) => {
+							onAddDueDate(event)
+						}}
+						className="btn-dates save"
+					>
+						Save
+					</button>
+					<button
+						onClick={(event) => {
+							onRemoveDueDate(event)
+						}}
+						className="clean-btn btn-dates remove"
+					>
+						Remove
+					</button>
+				</div>
+			</section>
+		</>
+	)
+}
--- Frontend/src/cmps/task-details-cmp/task-cmp-dynamic.jsx
+++ Frontend/src/cmps/task-details-cmp/task-cmp-dynamic.jsx
@@ -3,111 +3,119 @@ import { useSelector } from 'react-redux'
 
 import { TaskCoverModal } from './task-cmp-dynamic-modals/task-cover-modal'
 
-import { IoClose } from "react-icons/io5"
+import { IoClose } from 'react-icons/io5'
 import { BiCheck } from 'react-icons/bi'
 import Loader from '../../assets/img/loader.svg'
 import { saveTask } from '../../store/board.actions'
 import { TaskList } from '../task-preview/task-list'
 import { TaskLabelModal } from './task-cmp-dynamic-modals/task-label-modal'
 import { TaskMemberModal } from './task-cmp-dynamic-modals/task-member-modal'
+import { TaskDatesModal } from './task-cmp-dynamic-modals/task-dates-modal'
 
-export function TaskCmpDynamoic({ cmpType, task, onOpenModal, boardId, groupId, refDataBtn, onSaveTask }) {
+export function TaskCmpDynamoic({
+	cmpType,
+	task,
+	onOpenModal,
+	boardId,
+	groupId,
+	refDataBtn,
+	onSaveTask,
+}) {
+	const board = useSelector((storeState) => storeState.boardModule.board)
+	const [isAddLabelModalOpen, setIsAddLabelModalOpen] = useState(false)
+	const [selectedLabel, setSelectedLabel] = useState('')
+	// const members = board.members
+	// const labels = board.labels
 
-    const board = useSelector((storeState) => storeState.boardModule.board)
-    const [isAddLabelModalOpen, setIsAddLabelModalOpen] = useState(false)
-    const [selectedLabel, setSelectedLabel] = useState('')
-    // const members = board.members
-    // const labels = board.labels
+	// const [updateTask, setUpdateTask] = useState(task)
+	// const [toRender, setToRender] = useState(members)
+	// const [toRender, setToRender] = useState(labels)
 
-    // const [updateTask, setUpdateTask] = useState(task)
-    // const [toRender, setToRender] = useState(members)
-    // const [toRender, setToRender] = useState(labels)
+	const modalPos = {
+		top: refDataBtn.current.offsetTop + 'px',
+		left: refDataBtn.current.offsetLeft + 'px',
+	}
 
-    const modalPos = {
-        top: refDataBtn.current.offsetTop + "px",
-        left: refDataBtn.current.offsetLeft + "px"
-    }
+	function onClose() {
+		onOpenModal()
+	}
 
+	let data = {
+		title: cmpType,
+		txt: '',
+		placeholder: `Search ${cmpType}`,
+		optionsTitle: `Board ${cmpType}`,
+	}
 
+	if (cmpType === 'labels' && isAddLabelModalOpen) {
+		if (!selectedLabel) data.title = 'Create Label'
+		else data.title = 'Edit Label'
+	}
 
+	// async function onToggleLabel(id) {
+	//     if (labelIds?.includes(id)) {
+	//         const index = labelIds.indexOf(id)
+	//         labelIds.splice(index, 1)
+	//     }
+	//     else {
+	//         if (labelIds) labelIds.push(id)
+	//         else labelIds = [id]
+	//     }
+	//     setUpdateTask((prevTask) => ({ ...prevTask }))
+	//     await saveTask(updateTask, groupId, boardId)
+	// }
 
-    function onClose() {
-        onOpenModal()
-    }
+	// function handleChange({ target }) {
+	//     const regex = new RegExp(target.value, 'i')
+	//     const filteredMembers = members.filter((member) => regex.test(member.fullname))
+	//     console.log('filteredMembers:', filteredMembers)
 
-    let data = {
-        title: cmpType,
-        txt: '',
-        placeholder: `Search ${cmpType}`,
-        optionsTitle: `Board ${cmpType}`
+	//     setToRender(filteredMembers)
+	// }
 
-    }
+	if (!board)
+		return (
+			<div className="loader-wrapper">
+				<img className="loader" src={Loader} alt="loader" />
+			</div>
+		)
 
-    if (cmpType === 'labels' && isAddLabelModalOpen) {
+	return (
+		<div className="task-cmp-dynamoic" style={modalPos}>
+			<div className="task-cmp-dynamoic-container">
+				<a onClick={onClose}>
+					<IoClose className="close-icon" />
+				</a>
+				<p className="cmp-dynamoic-title">{data.title}</p>
+				<div className="dynamic-container">
+					{cmpType === 'members' && (
+						<TaskMemberModal task={task} data={data} onSaveTask={onSaveTask} />
+					)}
 
-        if (!selectedLabel) data.title = 'Create Label'
-        else data.title = 'Edit Label'
+					{cmpType === 'labels' && (
+						<TaskLabelModal
+							task={task}
+							data={data}
+							onSaveTask={onSaveTask}
+							setIsAddLabelModalOpen={setIsAddLabelModalOpen}
+							isAddLabelModalOpen={isAddLabelModalOpen}
+							selectedLabel={selectedLabel}
+							setSelectedLabel={setSelectedLabel}
+						/>
+					)}
 
-    }
+					{cmpType === 'cover' && <TaskCoverModal task={task} />}
 
-
-    // async function onToggleLabel(id) {
-    //     if (labelIds?.includes(id)) {
-    //         const index = labelIds.indexOf(id)
-    //         labelIds.splice(index, 1)
-    //     }
-    //     else {
-    //         if (labelIds) labelIds.push(id)
-    //         else labelIds = [id]
-    //     }
-    //     setUpdateTask((prevTask) => ({ ...prevTask }))
-    //     await saveTask(updateTask, groupId, boardId)
-    // }
-
-    // function handleChange({ target }) {
-    //     const regex = new RegExp(target.value, 'i')
-    //     const filteredMembers = members.filter((member) => regex.test(member.fullname))
-    //     console.log('filteredMembers:', filteredMembers)
-
-    //     setToRender(filteredMembers)
-    // }
-
-
-    if (!board) return <div className="loader-wrapper"><img className="loader" src={Loader} alt="loader" /></div>
-
-    return <div className='task-cmp-dynamoic' style={modalPos}>
-        <div className='task-cmp-dynamoic-container'>
-            <a onClick={onClose}><IoClose className='close-icon' /></a>
-            <p className='cmp-dynamoic-title'>{data.title}</p>
-            <div className='dynamic-container'>
-
-                {cmpType === 'members' &&
-                    <TaskMemberModal
-                        task={task}
-                        data={data}
-                        onSaveTask={onSaveTask}
-
-                    />}
-
-                {cmpType === 'labels' &&
-                    <TaskLabelModal
-                        task={task}
-                        data={data}
-                        onSaveTask={onSaveTask}
-                        setIsAddLabelModalOpen={setIsAddLabelModalOpen}
-                        isAddLabelModalOpen={isAddLabelModalOpen}
-                        selectedLabel={selectedLabel}
-                        setSelectedLabel={setSelectedLabel}
-                    />}
-
-                {cmpType === 'cover' &&
-                    <TaskCoverModal
-                        task={task}
-                    />}
-
-            </div>
-        </div>
-    </div>
-
-
+					{cmpType === 'dates' && (
+						<TaskDatesModal
+							task={task}
+							data={data}
+							onSaveTask={onSaveTask}
+							onClose={onClose}
+						/>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
