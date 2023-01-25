081dd39be26b748555147c71cc4c8ced3db93b9a
beta0022 <114179647+beta0022@users.noreply.github.com>
2023-01-25 09:17:10 +0200

    Loader fixed

--- Frontend/src/cmps/group/group-list.jsx
+++ Frontend/src/cmps/group/group-list.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
 import { boardService } from '../../services/board.service'
 import { removeGroup, saveGroup } from '../../store/board.actions'
+
 import { TaskList } from '../task-preview/task-list'
 import { GroupDropdown } from './group-dropdown'
 
@@ -91,7 +94,7 @@ export function GroupList() {
 		}
 	}
 
-	if (!groups) return <img className="loader" src={Loader} alt="loader" />
+	if (!groups) return <div className="loader-wrapper"><img className="loader" src={Loader} alt="loader" /></div>
 
 	const addNewTxt = groups.length === 0 ? 'Add a list' : 'Add another list'
 
